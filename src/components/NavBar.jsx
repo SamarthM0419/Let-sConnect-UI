c22879af6e4acba7f165504e05b31f750be1dced
samarth2534@gmail.com <samarth2534@gmail.com>
2024-11-25 09:16:50 +0530

    Refactoring of the code

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const user = useSelector((state) => state.user);
@@ -7,11 +8,11 @@ const NavBar = () => {
   return (
     <div className="navbar bg-base-300">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">🧑‍💻 Let's Match</a>
+        <Link to="/" className="btn btn-ghost text-xl">🧑‍💻 Let's Match</Link>
       </div>
       {user && (
         <div className="flex-none gap-2">
-          <div className="form-control">Welcome, {user.data.firstName}</div>
+          <div className="form-control">Welcome, {user?.data?.firstName}</div>
 
           <div className="dropdown dropdown-end mx-5 flex">
             <div
@@ -20,7 +21,7 @@ const NavBar = () => {
               className="btn btn-ghost btn-circle avatar"
             >
               <div className="w-10 rounded-full">
-                <img alt="user photo" src={user.data.photourl} />
+                <img alt="user photo" src={user?.data?.photourl} />
               </div>
             </div>
             <ul
@@ -28,10 +29,10 @@ const NavBar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="/profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
