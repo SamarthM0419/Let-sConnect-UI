be8374ccbe466644b731e9b7e3c6a8748e3f1988
samarth2534@gmail.com <samarth2534@gmail.com>
2024-11-26 09:19:06 +0530

    created profile

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 
 const UserCard = ({ user }) => {
+  const { firstName, lastName, age, gender, about, photoUrl, skills } = user;
 
-  const { firstName, lastName, age, gender, about, photoUrl } = user;
-  
   return (
     <div className="card bg-base-300 w-64 sm:w-72 md:w-80 shadow-xl mx-auto">
       <figure>
@@ -14,11 +13,14 @@ const UserCard = ({ user }) => {
         />
       </figure>
       <div className="card-body p-4">
-        <h2 className="card-title text-base sm:text-lg">{firstName + " " + lastName}</h2>
-        {age && gender && (
-          <p className="text-sm">{`${age}, ${gender}`}</p>
+        <h2 className="card-title text-base sm:text-lg">
+          {firstName + " " + lastName}
+        </h2>
+        {age && gender && skills && (
+          <p className="text-sm">{`${age}, ${gender} , ${skills}`}</p>
         )}
         <p className="text-xs">{about}</p>
+
         <div className="card-actions justify-center my-2 space-x-2">
           <button className="btn btn-primary btn-sm">Ignore</button>
           <button className="btn btn-secondary btn-sm">Interested</button>
